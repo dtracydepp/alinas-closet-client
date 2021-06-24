6a88e9725a9f88b28ec56751c8642137e2e4c780
dtracydepp <tracydepp82@gmail.com>
2021-06-24 15:47:20 -0500

    updated home.js and app.css

--- src/Home.js
+++ src/Home.js
@@ -8,7 +8,7 @@ export const Home = () => (
     <>
         <PropsAndState yourName={"Snoh"} />
        <div className="home__pic"><img src={Alina} alt="Alina" /></div>
-       <div className="home__tag"><medium>Our little ones need lots of time, shop smarter..</medium></div>
+       <div className="home__tag"><p>Our little ones need lots of time, shop smarter..</p></div>
        
     </>
 )
